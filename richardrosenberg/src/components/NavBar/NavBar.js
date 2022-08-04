54f040c5b8537b5a69aae60bea025880ad7ceb2a
Richard Rosenberg <rosenbr73@gmail.com>
2022-08-04 09:08:54 -0700

    random commit

--- richardrosenberg/src/components/NavBar/NavBar.js
+++ richardrosenberg/src/components/NavBar/NavBar.js
@@ -1,30 +1,30 @@
 import './NavBar.css';
 import Dropdown from '../Dropdown/Dropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const NavBar = () => {
-// TODO make navbar the hamburger, when clicked displays all the below
-    const [show, setShow] = useState(false);
-    const showDropdown = (e) => {
-        setShow(!show);
-    }
-    const hideDropdown = (e) => {
-        setShow(false);
-    }
+// // TODO make navbar the hamburger, when clicked displays all the below
+//     const [show, setShow] = useState(false);
+//     const showDropdown = (e) => {
+//         setShow(!show);
+//     }
+//     const hideDropdown = (e) => {
+//         setShow(false);
+//     }
 
     return (
         <nav>
             <div className='navbar__burger'
-                renderMenuOnMount={true}
-                show={show}
-                onMouseEnter={showDropdown} 
-                onMouseLeave={hideDropdown}
+                // renderMenuOnMount={true}
+                // show={show}
+                // onMouseEnter={showDropdown} 
+                // onMouseLeave={hideDropdown}
             >
                 <FontAwesomeIcon icon="fa-solid fa-bars">
-                        {show && <Dropdown />}
-                        {/* <Dropdown /> */}
+                        {/* {show && <Dropdown />} */}
                 </FontAwesomeIcon>
+                        <Dropdown />
             </div>
         </nav>
     )
